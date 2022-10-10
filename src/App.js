c49ac4d65cf3d2b261907492d5b978b55330769a
ccatack <59943562+ccatack@users.noreply.github.com>
2022-10-09 18:53:25 -0700

    created header, navbar, and page placeholders and created logic for switching pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from "react";
 
 export default function App() {
+
+  const [page, setPage] = React.useState(0)
+
+  function handlePageChange(event) {
+    setPage(event.target.value)
+  }
+
   return (
-    <div>
-      insert blog here lol
+    <div className="app-container">
+      <header>Corb's Art Blog</header>
+      <nav>
+        <button onClick={handlePageChange} value="0">most recent post</button>
+        <button onClick={handlePageChange} value="1">landscapes</button>
+        <button onClick={handlePageChange} value="2">sprites</button>
+      </nav>
+      {page == 0 && 
+        <div className="recent-post">
+          recent post page
+        </div>
+      }
+
+      {page == 1 && 
+        <div className="landscapes">
+          landscapes page
+        </div>
+      }
+
+      {page == 2 && 
+        <div className="sprites">
+          sprites page
+        </div>
+      }
     </div>
   );
 }
