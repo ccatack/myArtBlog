de5beda06f2fa84a3b5e20d85dd6432c53cf0ae6
ccatack <59943562+ccatack@users.noreply.github.com>
2022-10-10 20:22:02 -0700

    added some styling for buttons and header

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ export default function App() {
 
   return (
     <div className="app-container">
-      <header>Corb's Art Blog</header>
-      <nav>
-        <button onClick={handlePageChange} value="0">most recent post</button>
-        <button onClick={handlePageChange} value="1">landscapes</button>
-        <button onClick={handlePageChange} value="2">sprites</button>
+      <header>
+        <h1>Corb's Art Blog</h1>
+      </header>
+      <nav className="button-container">
+        <button className="nav-button" onClick={handlePageChange} value="0">most recent post</button>
+        <button className="nav-button" onClick={handlePageChange} value="1">landscapes</button>
+        <button className="nav-button" onClick={handlePageChange} value="2">sprites</button>
       </nav>
       {page == 0 && 
         <div className="recent-post">
