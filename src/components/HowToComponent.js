15d77015c08d2e972fb8b8b4aab550b38f5dbda1
ccatack <59943562+ccatack@users.noreply.github.com>
2022-11-04 11:37:53 -0700

    howTo section scrolls through images properly and back button disables at correct time

--- src/components/HowToComponent.js
+++ src/components/HowToComponent.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function HowToComponent() {
 
+    const backButtonRef = React.useRef()
+
     const [slide, setSlide] = React.useState(0)
     const ref0 = React.useRef()
     const ref1 = React.useRef()
@@ -9,12 +11,12 @@ export default function HowToComponent() {
     const refArray = [ref0, ref1]
 
     React.useEffect(() => {
-        showIMGs()
+        handleIMGChange(0)
     }, [])
 
-    const showIMGs = () => {
+    const showIMGs = (current) => {
         for (var i = 0; i < refArray.length; i++) {
-            if (refArray[i].current.id == slide) {
+            if (refArray[i].current.id == current) {
                 refArray[i].current.style.display = "block"
             }
             else {
@@ -24,23 +26,31 @@ export default function HowToComponent() {
     }
 
     function handleIMGChange(n) {
-        setSlide((prevSlide) => {
-            if (prevSlide + n >= refArray.length) {
-                return 0
+        var current = slide
+        if (current + n >= refArray.length) {
+            backButtonRef.current.disabled = true
+            current = 0
+        }
+        else {
+            if (current + n <= 0) {
+                backButtonRef.current.disabled = true
+                current = 0
             }
             else {
-                return prevSlide + n
+                backButtonRef.current.disabled = false
+                current =+ n
             }
-        })
-        console.log(slide)
-        showIMGs()
+        }
+        setSlide(current)
+        console.log(current)
+        showIMGs(current)
     }
 
     return (
         <div className="how-to-container">
             <div>How to create pixel art</div>
             <div className="image-button-container">
-                <button onClick={() => handleIMGChange(-1)}>back</button>
+                <button ref={backButtonRef} onClick={() => handleIMGChange(-1)}>back</button>
                 <img ref={ref0} id="0" src="../images/howTo/spead1.png"></img>
                 <img ref={ref1} id="1" src="../images/howTo/spread2.png"></img>
                 <button onClick={() => handleIMGChange(+1)}>next</button>
